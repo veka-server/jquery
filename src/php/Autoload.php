7655b1cf13696633ea36d7a1ba26fef4bd75f9e4
Veka <veka610@gmail.com>
2022-03-11 22:04:01 +0100

    Update Autoload.php

--- src/php/Autoload.php
+++ src/php/Autoload.php
@@ -2,18 +2,20 @@
 
 namespace VekaServer\Jquery;
 
-class Autoload
+use VekaServer\Interfaces\PluginInterface;
+
+class Autoload implements PluginInterface
 {
 
-    public static function getPathView(){
+    public static function getPathView():array{
         return [];
     }
 
-    public static function getPathJS(){
+    public static function getPathJS():array{
         return dirname(__DIR__).DIRECTORY_SEPARATOR.'js';
     }
 
-    public static function getPathCSS(){
+    public static function getPathCSS():array{
         return null;
     }
 
